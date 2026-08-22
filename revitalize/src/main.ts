1ed990d011803eb2c3ef822bc52383fe0ea9b4ab
LawftyGoals <mymsnadressis@hotmail.com>
2026-08-23 00:45:45 +0200

    some refactor and working on attacks now, collision is ok

--- revitalize/src/main.ts
+++ revitalize/src/main.ts
@@ -10,38 +10,39 @@ function getDebug(innerHtml: string) {
   if (frameCount >= 10) frameCount = 0;
 }
 type Position = {
-  x: number;
-  y: number;
+  x: number,
+  y: number,
 };
 
 type Size = {
-  h: number;
-  w: number;
+  h: number,
+  w: number,
   halfSizeH: number,
   halfSizeW: number
 };
 
 type BgSize = {
-  h: number;
-  w: number;
-};
-
-type RigidBody = {
   h: number,
-  w: number,
-}
+  w: number
+};
 
 type Thing = {
   id: number,
+  hp: number;
+  maxHp: number;
   speed: number,
+  slowed: number,
   moving: boolean,
   position: Position,
+  distanceX: number,
+  distanceY: number,
   size: Size,
+  collisionLayer: Set<number>,
+  targetCollisionLayer: Set<number>,
   color: string,
   targetPosition: Position,
   rotationTarget: Position,
   rotation: number,
-  rigidBody: RigidBody
 }
 
 type Zone = {
@@ -50,6 +51,10 @@ type Zone = {
   size: BgSize
 };
 
+const initThingsNotPlayer = 100;
+const CurrentNumberOfThings = 1 + initThingsNotPlayer;
+
+function createPlayerAttack()
 
 const defaultZoneSize = {h: 720, w: 1280};
 
@@ -73,39 +78,58 @@ let paused = true;
 
 const mousePosition = { x: defaultZoneSize.w/2, y: defaultZoneSize.h/2 };
 
+/*
+  * collision layer: 
+  * 0 - player
+* 1 - base enemy
+*/
+
 const player: Thing = {
   id: 0,
+  hp: 500,
+  maxHp: 500,
   speed: 300,
+  slowed: 0,
   moving: false,
   position: {
     x: defaultZoneSize.w / 2,
     y: defaultZoneSize.h / 2,
   },
+  collisionLayer: new Set([0]),
+  targetCollisionLayer: new Set([1]),
+  distanceX: 0,
+  distanceY: 0,
   size: {h: 30, w: 30, halfSizeH: 15, halfSizeW: 15},
   color: "red",
   targetPosition:  {x: defaultZoneSize.w / 2, y: defaultZoneSize.h /2},
   rotationTarget: mousePosition,
   rotation: 0,
-  rigidBody: {h: 30, w: 30}
 };
 
-
-const things: Thing[] = [player];
+const thingsSize = 1001;
+const things: Thing[] = new Array(thingsSize);
+things[0] = player;
 
 function randomThingCreator(count: number){
-  for(let i = 0; i < count; i++){
-    things.push(
+  for(let i = 1; i < count; i++){
+    things[i] = (
       {
-        id: i + 1,
+        id: i,
+        hp: 50,
+        maxHp: 50,
         speed: 170,
+        slowed: 0,
         moving: true,
+        distanceX: 0,
+        distanceY: 0,
         position: {x: Math.floor(Math.random()*defaultZoneSize.w), y: Math.floor(Math.random()*defaultZoneSize.h)},
         size: {h: 20, w: 20, halfSizeH: 10, halfSizeW: 10},
+        collisionLayer: new Set([1]),
+        targetCollisionLayer: new Set([0, 1]),
         color: `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`,
         targetPosition: player.position,
         rotationTarget: player.position,
         rotation: 0,
-        rigidBody: {h: 30, w: 30}
       }
     )
   }
@@ -121,19 +145,21 @@ function playerZoneDisplace(){
   return {displaceX: (player.position.x - defaultZoneSize.w/2), displaceY: (player.position.y - defaultZoneSize.h/2)};
 }
 
-function fR(
+function drawThing(
   ctx: CanvasRenderingContext2D,
   thing: Thing,
   position: Position,
   displace: {displaceX: number, displaceY: number}
 ) {
-  const {size: {h, w}, color, rotation} = thing;
+  const {size: {h, w, halfSizeW, halfSizeH}, color, rotation} = thing;
   const {x, y} = position;
   const {displaceX, displaceY} = displace;
   ctx.fillStyle = color;
   ctx.translate(x - displaceX, y - displaceY);
   ctx.rotate(rotation);
+  ctx.translate(-halfSizeW, -halfSizeH);
   ctx.fillRect(0, 0, w, h);
+  ctx.translate(halfSizeW, halfSizeH);
   ctx.rotate(-(rotation));
   ctx.translate(-(x - displaceX), - (y - displaceY));
 }
@@ -145,7 +171,7 @@ function positionZoneConverter({x, y}: Position){
   return {x: convX, y: convY};
 }
 
-function fRMbg(
+function drawBg(
   ctx: CanvasRenderingContext2D,
   zone: Zone 
 ){
@@ -199,6 +225,23 @@ function getEdges(position: Position, size: Size): Edges{
   }
 }
 
+function getDistanceFromThing(elapsedS: number, thing: Thing, targetPosition: Position){
+
+  const omx = thing.moving ? (targetPosition.x - thing.position.x) : 0;
+  const omy = thing.moving ? (targetPosition.y - thing.position.y) : 0;
+
+  const magdeb = Math.sqrt(omx * omx + omy * omy);
+
+  const nmx = magdeb >= 1 ? omx / magdeb : 0;
+  const nmy = magdeb >= 1 ? omy / magdeb : 0;
+
+  const velocityX = thing.speed * nmx * (1 - thing.slowed);
+  const velocityY = thing.speed * nmy * (1 - thing.slowed);
+  
+  return {x: velocityX * elapsedS, y: velocityY * elapsedS}
+}
+
+
 function collisionDetector(thingA: Thing, thingANewPos: Position, thingB: Thing ){
   const {t: at, b: ab, l: al, r: ar} = getEdges(thingANewPos, thingA.size);
   const {t: bt, b: bb, l: bl, r: br} = getEdges(thingB.position, thingB.size);
@@ -206,71 +249,93 @@ function collisionDetector(thingA: Thing, thingANewPos: Position, thingB: Thing 
   return !(ab < bt || at > bb || ar < bl || al > br);
 }
 
-function move(elapsedMS: number, thing: Thing){
 
-  const omx = thing.moving ? (thing.targetPosition.x - thing.position.x) : 0;
-  const omy = thing.moving ? (thing.targetPosition.y - thing.position.y) : 0;
 
-  const magdeb = Math.sqrt(omx * omx + omy * omy);
+function action(elapsedS: number, thing: Thing){
 
-  const nmx = magdeb >= 1 ? omx / magdeb : 0;
-  const nmy = magdeb >= 1 ? omy / magdeb : 0;
+  let distanceX = 0;
+  let distanceY = 0;
+  
+  if(thing.position.x === thing.targetPosition.x && thing.position.y === thing.targetPosition.y){
+    distanceX = 0;
+    distanceY = 0;
+  } else {
+    ({x: distanceX, y: distanceY} = getDistanceFromThing(elapsedS, thing, thing.targetPosition));
+  }
 
-  let movementX = (thing.speed * elapsedMS * nmx);
-  let movementY = (thing.speed * elapsedMS * nmy);
+  //TODO: REMOVE PLEASE
+  thing.slowed = 0;
 
   for(let idx = 0; idx < things.length; idx++){
     const otherThing = things[idx];
     if(thing.id !== otherThing.id){
       let thingX = thing.position.x;
       let thingY = thing.position.y;
-      const collisionDetected = collisionDetector(thing, {x: thingX += movementX, y: thingY += movementY}, things[idx]);
+      const collisionDetected = collisionDetector(thing, {x: thingX += distanceX, y: thingY += distanceY}, things[idx]);
       if(collisionDetected){
-        /*const distanceBetweenThings = Math.sqrt(thingX * thingX + thingY * thingY);
-        const cutDistance = distanceBetween - thing.size.w - otherThing.size.w;
-*/      movementX = 0;
-        movementY = 0;
+        const dist = getDistanceFromThing(elapsedS, thing, otherThing.position);
+        if(otherThing.id === 0){
+          //TODO: MOVE SLOW INTO ENEMY ATTACK
+          otherThing.slowed = 0.7;
+          if(Math.abs(dist.x) < 12 && Math.abs(dist.y) < 12){
+            distanceX = 0;
+            distanceY = 0;
+          }
+          else {
+            distanceX = -(distanceX*2);
+            distanceY = -(distanceY*2);
+          }
+        }
+        else if (thing.id !== 0){
+          if(Math.abs(dist.x) < 0.5 && Math.abs(dist.y) < 0.5){
+            dist.x = thing.size.w;
+            dist.y = thing.size.h;
+          }
+          distanceX = -dist.x;
+          distanceY = -dist.y;
+        }
       }
-
-      //if(collisionDetected)getDebug(`collision detected: thing ${thing.id} - thing ${things[idx].id}`)
     }
   }
 
+  return {distanceX, distanceY};
 
-  return {movementX, movementY};
   
 }
 
 
-function moveThings(elapsedMS: number, thing: Thing) {
+function attack(){
+  
+  
+  return 0;
+}
 
-  const {movementX, movementY} = move(elapsedMS, thing);
 
-  thing.position.x += movementX;
-  thing.position.y += movementY ;
-
+function moveThings(elapsedS: number, thing: Thing) {
+  const {distanceX, distanceY} = action(elapsedS, thing);
+  thing.position.x += distanceX;
+  thing.position.y += distanceY;
+  thing.distanceX = distanceX;
+  thing.distanceY = distanceY;
 }
 
 function rotatospotatos(thing: Thing, position: Position, rotationTarget: Position){
   thing.rotation = (Math.atan2(rotationTarget.y - position.y, rotationTarget.x - position.x))-(Math.PI/4) ;
-  //if(thing.id === 0) getDebug(`${rotationTarget.x} ${rotationTarget.y} - ${position.x} ${position.y} ${thing.rotation}`);
 }
 
-function draw(ctx: CanvasRenderingContext2D, prevTime: number, timestamp: number) {
+function run(ctx: CanvasRenderingContext2D, prevTime: number, timestamp: number) {
   const elapsed = timestamp - prevTime;
-  const elapsedMS = elapsed / 1000;
+  const elapsedS = elapsed / 1000;
   if(!paused){
-
-
     //CALCULATIONS AND PHYSICS
     setPlayerTarget();
-    moveThings(elapsedMS, player);
+    moveThings(elapsedS, player);
     rotatospotatos(player, playerCentered, player.rotationTarget);
 
     const displace = playerZoneDisplace();
     for(let idx = 1; idx < things.length; idx++){
       const thing = things[idx];
-      moveThings(elapsedMS, thing);
+      if(thing.moving) moveThings(elapsedS, thing);
       rotatospotatos(thing, thing.position, thing.rotationTarget);
     };
 
@@ -278,16 +343,16 @@ function draw(ctx: CanvasRenderingContext2D, prevTime: number, timestamp: number
     //RENDER
     ctx.clearRect(0,0, defaultZoneSize.w, defaultZoneSize.h);
     map.forEach((zone)=> {
-      fRMbg(ctx, zone);
+      drawBg(ctx, zone);
     });
-    fR(ctx, player, playerCentered, {displaceX: 0, displaceY: 0});
+    drawThing(ctx, player, playerCentered, {displaceX: 0, displaceY: 0});
     for(let idx = 1; idx < things.length; idx++){
       const thing = things[idx];
-      fR(ctx, thing, thing.position, displace);
+      drawThing(ctx, thing, thing.position, displace);
     };
   }
 
-  requestAnimationFrame((ts) => draw(ctx, timestamp, ts));
+  requestAnimationFrame((ts) => run(ctx, timestamp, ts));
 }
 
 function addEL(canvas: HTMLCanvasElement) {
@@ -351,9 +416,10 @@ function init() {
     );
     return 0;
   }
-  randomThingCreator(10);
+  
+  randomThingCreator(initThingsNotPlayer);
 
-  requestAnimationFrame((timestamp) => draw(ctx, 0, timestamp));
+  requestAnimationFrame((timestamp) => run(ctx, 0, timestamp));
 }
 
 init();
